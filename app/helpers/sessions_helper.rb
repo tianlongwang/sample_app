0c1df640cf1eb46c31e598b5731bf2d12c370f2b
Tian Long Wang <tianlong.wang.13@googlemail.com>
2012-07-27 20:42:11 -0400

    9.2.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,6 +15,9 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
+  def current_user?(user)
+    user == current_user
+  end
 
   def sign_out
     self.current_user = nil
